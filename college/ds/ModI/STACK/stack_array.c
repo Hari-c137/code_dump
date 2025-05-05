3102356d1eee8a11d4f80c5adb5d6d1bd6fb7833
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-05-05 23:19:50 +0530

    restructured

--- college/ds/ModI/STACK/stack_array.c
+++ college/ds/ModI/STACK/stack_array.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #define MAX 128
 
-typedef struct { int items[MAX]; int top; } IntStack; // create a callname 'IntStack' for easier reference to struct
+typedef struct {int items[MAX]; int top;} Stack; 
 
+void push(Stack *s, int val) { s->top <= MAX -1 ? s->items[++s->top] = val : printf("overflow \n"); }
+int  peek(Stack *s) { return s->top >= 0 ? s->items[s->top] : printf("empty \n");       }
+void show(Stack *s) { for (int i = s->top; i >= 0; i--) printf("%d \n", s->items[i]);   }
+int  poop(Stack *s) { return s->top >= 0 ? s->items[s->top--] : printf("underflow \n"); }
 
-// documention for this part of code is already provided in postfix section
-void push(IntStack *s, int v) { s->top < MAX - 1 ? s->items[++s->top] = v : printf("Overflow\n"); } 
-int pop(IntStack *s) { return s->top >= 0 ? s->items[s->top--] : (printf("Underflow\n"), -1); }
-int peek(IntStack *s) { return s->top >= 0 ? s->items[s->top] : (printf("Empty\n"), -1); }
-void display(IntStack *s) { for(int i = s->top; i >= 0; i--) printf("%d\n", s->items[i]); }
+void main() {
 
-int main() {
-  IntStack s = {.top = -1}; int ch, val; // inialize stack 's' through instance of the structure 'IntStack' and set a var 'top' with -1 (MT)
-  while (1) { // inifinity while loop mutha-fuckassss
-    printf("1.Push 2.Pop 3.Peek 4.Display 5.Exit: "); scanf("%d", &ch);
-    if (ch == 1) { printf("Val: "); scanf("%d", &val); push(&s, val); }
-    else if (ch == 2) { val = pop(&s); if (val != -1) printf("Popped: %d\n", val); }
-    else if (ch == 3) { val = peek(&s); if (val != -1) printf("Top: %d\n", val); }
-    else if (ch == 4) { display(&s); }
+  Stack s = {.top = -1 }; int ch, val;
+  while(1) {
+    printf("1.push 2.pop 3.peek 4.show 5.exit "); scanf("%d", &ch);
+    if(ch == 1) { printf("Val: "); scanf("%d", &val); push(&s, val); }
+    else if (ch == 2) { val = poop(&s); printf("%d\n", val); }
+    else if (ch == 3) { val = peek(&s); printf("%d\n", val); }
+    else if (ch == 4) { show(&s); }
     else if (ch == 5) break;
-    else printf("Invalid\n");
+    else {printf("invalid \n");}
   }
 }
-
