22c9e0446bf8c4e0cd03454b12fe441bafd61db4
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-09-02 23:44:36 +0530

    newer implementation of guess_number

--- rust/rust-book/00-02/guess_number_harder/src/main.rs
+++ rust/rust-book/00-02/guess_number_harder/src/main.rs
@@ -1,3 +1,26 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let randno = rand::rng().random_range(1..=100);
+    println!("melcow to the number_guesser");
+    loop {
+        let mut guess = String::new();
+        println!("enter your guess");
+        io::stdin().read_line(&mut guess).expect("cant read_line");
+        println!("your guess is {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&randno) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too large"),
+            Ordering::Equal => {
+                println!("u win!!");
+                break;
+            }
+        }
+    }
 }
