a142a3eb0fa2edab871bbb16e9aabc88f9c06432
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-09-28 23:12:54 +0530

    simple calculator in rust
    for writing this, i should be sentenced to death

--- rust/rust-book/00-02/simple-calc/src/main.rs
+++ rust/rust-book/00-02/simple-calc/src/main.rs
@@ -1,3 +1,39 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut n1 = String::new();
+    let mut n2 = String::new();
+
+    enum Op {
+        Add,
+        Dif,
+        Mul,
+        Div,
+    }
+
+    io::stdin().read_line(&mut n1).expect("error");
+    io::stdin().read_line(&mut n2).expect("error");
+
+    let n1: u32 = match n1.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return,
+    };
+    let n2: u32 = match n2.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return,
+    };
+
+    fn cal(n1: u32, n2: u32, op: Op) {
+        match op {
+            Op::Add => println!("sum is {:?}", n1 + n2),
+            Op::Dif => println!("dif is {:?}", n1 - n2),
+            Op::Mul => println!("mul is {:?}", n1 * n2),
+            Op::Div => println!("div is {:?}", n1 / n2),
+        }
+    }
+
+    cal(n1, n2, Op::Add);
+    cal(n1, n2, Op::Dif);
+    cal(n1, n2, Op::Mul);
+    cal(n1, n2, Op::Div);
 }
