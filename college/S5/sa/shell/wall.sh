d7fb9fc0d480eafb03ee54774e1b2b5229ab8dad
ClearlyOpaque <hariprasadaji465@gmail.com>
2025-10-22 18:50:14 +0530

    removed random-wall feature

--- college/S5/sa/shell/wall.sh
+++ college/S5/sa/shell/wall.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers" 
-INTERVAL=1200  # 20 minutes in seconds
 
-if ! command -v gsettings >/dev/null 2>&1; then
-    echo "Err: not on GNOME DE"
-    exit 1
-fi
 
 get_wallpapers() {
     find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | sort
@@ -20,23 +15,12 @@ show_menu() {
         echo "  [$i] $name"
         i=$((i + 1))
     done
-    echo "  [r] Random wallpaper mode (change every 20 minutes)"
     echo "  [q] Quit"
 }
 
 set_wallpaper() {
     gsettings set org.gnome.desktop.background picture-uri "file://$1"
-    gsettings set org.gnome.desktop.background picture-uri-dark "file://$1" 2>/dev/null
     echo "Wallpaper set to: $(basename "$1")"
-}
-
-random_mode() {
-    echo "Random wallpaper mode started. Changing every $(($INTERVAL / 60)) minutes..."
-    while true; do
-        wp=$(echo "$wallpapers" | shuf -n 1)
-        set_wallpaper "$wp"
-        sleep "$INTERVAL"
-    done
 }
 
 wallpapers=$(get_wallpapers)
@@ -56,8 +40,6 @@ read choice
 if [ "$choice" = "q" ]; then
     echo "Exiting."
     exit 0
-elif [ "$choice" = "r" ]; then
-    random_mode
 elif [ "$choice" -ge 1 ] 2>/dev/null && [ "$choice" -le "$count" ]; then
     selected=$(echo "$wallpapers" | sed -n "${choice}p")
     set_wallpaper "$selected"
